47c72cbbfbe0fd9189104eb2ff51faabb5f58ced
Junior Felix Gervacio Burgos <juniorfelixgb@gmail.com>
2021-03-24 20:25:02 -0400

    first step

---
+++ ChatRealTime/ChatRealTime/wwwroot/js/app/chat/chatHub.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+const conexion = new signalR.HubConnectionBuilder().withUrl("/chathub").build();
+
+conexion.on("GetMessage", (user, message) => {
+    let li = document.createElement("li");
+    li.textContent = `${user} - ${message}`;
+    document.getElementById("lstMessages").appendChild(li);
+});
+
+conexion.start().then(() => {
+    let li = document.createElement('li');
+    li.textContent = `Bienvenido al chat`;
+    document.getElementById("lstMessages").appendChild(li);
+}).catch((error) => {
+    console.error(error);
+})
+
+document.getElementById("btnSend").addEventListener("click", (e) => {
+    let user = document.getElementById("txtUser").value;
+    let message = document.getElementById("txtMessage").value;
+    conexion.invoke("SendMessage", user, message).catch((error) => {
+        console.error(error);
+    });
+    user.value = "";
+    message.value = "";
+    event.preventDefault();
+});
