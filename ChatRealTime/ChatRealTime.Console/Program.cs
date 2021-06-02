d05cf6863cd30b73f9b453aea242dfa566ae346e
Junior Gervacio <juniorfelixgb@gmail.com>
2021-06-02 13:49:21 -0400

    Last changes about chat real time console app

---
+++ ChatRealTime/ChatRealTime.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using ChatRealTimeConsole.Models;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace ChatRealTimeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.Sleep(2000);
+            string url = "http://localhost:49577/chatHub";
+            var connection = new HubConnectionBuilder().WithUrl(url)
+                                                       .Build();
+            connection.On<Message>("GetMessage", (message) =>
+            {
+                Console.WriteLine($"{message.User} - {message.Content}");
+            });
+            connection.StartAsync().Wait();
+            while (true)
+            {
+                var Content = Console.ReadLine();
+                connection.InvokeAsync("SendMessage", new Message() { User = "Consola", Content = Content });
+            }
+        }
+    }
+}
